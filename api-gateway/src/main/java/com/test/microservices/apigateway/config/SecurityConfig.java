642e7af7b61038afc93ec1388a5bfdc2fe8f3aa4
StudentPP1 <mishamb2007@gmail.com>
2025-07-28 16:51:36 +0300

    feat: add keycloak security for API Gateway

---
+++ api-gateway/src/main/java/com/test/microservices/apigateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.test.microservices.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    // ! default jwt & all endpoints are secured => config API Gateway as a resource server
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
+                .build();
+    }
+}
