e1b5bf67a204712f6a5bac0252c6fb281c83d0d4
StudentPP1 <mishamb2007@gmail.com>
2025-07-28 22:05:16 +0300

    chore: change access to rest api docs of services from them to API Gateway

--- api-gateway/src/main/java/com/test/microservices/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/test/microservices/apigateway/config/SecurityConfig.java
@@ -9,11 +9,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {
+            "/swagger-ui.html", "/swagger-ui/**",
+            "/v3/api-docs/**", "/swagger-resources/**",
+            "/api-docs/**", "/aggregate/**",
+    };
+
     // ! default jwt & all endpoints are secured => config API Gateway as a resource server
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(freeResourceUrls).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
--- api-gateway/src/main/java/com/test/microservices/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/com/test/microservices/apigateway/routes/Routes.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
-import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.*;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @Configuration
@@ -38,5 +40,31 @@ public class Routes {
                 .before(uri("http://localhost:8082"))
                 .build();
     }
-}
 
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return route("product_service_swagger")
+                .route(path("/aggregate/product-service/v3/api-docs"), http())
+                .filter(setPath("/api-docs")) // ! /aggregate/product-service/v3/api-docs -> /api-docs
+                .before(uri("http://localhost:8080")) // ! add prefix
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return route("order_service_swagger")
+                .route(path("/aggregate/order-service/v3/api-docs"), http())
+                .filter(setPath("/api-docs"))
+                .before(uri("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return route("inventory_service_swagger")
+                .route(path("/aggregate/inventory-service/v3/api-docs"), http())
+                .filter(setPath("/api-docs"))
+                .before(uri("http://localhost:8082"))
+                .build();
+    }
+}
